15da018e4b12179bbf19eb69aace6e97d1e68030
aanggaadc <aanggaadc@gmail.com>
2022-03-05 23:54:17 +0700

    creating middleware authentication for SuperAdmin role

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -21,7 +21,35 @@ const isLoggedIn = (req, res, next) => {
         req.flash('error', "You're not Login, Please Login")
         res.redirect('/login')
     }
-
 }
 
-module.exports = isLoggedIn
+const isLoggedInAsAdmin = (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (token) {
+      jwt.verify(token, "secret", (err, decodedToken) => {
+        if (err) {
+            req.flash('error', 'Token is Expired')
+            res.redirect('/login')
+        } else {
+          if (decodedToken.role !== 'SuperAdmin') {
+            req.flash('error', "You're Not Login As Admin")
+            res.redirect("/");
+          } else {
+            res.locals.user = decodedToken
+            console.log("Token Is :")
+            console.log(decodedToken)
+            next()
+          }
+        }
+      });
+    } else {
+        res.locals.user = null
+        req.flash('error', "You're not Login, Please Login")
+        res.redirect('/login')
+    }
+  }
+
+module.exports = {
+    isLoggedIn, 
+    isLoggedInAsAdmin
+}
--- router/index.js
+++ router/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router()
-const isLoggedIn = require('../middleware/authentication')
+const {
+    isLoggedIn,
+    isLoggedInAsAdmin
+} = require('../middleware/authentication')
 const{
     Index,
     SignUp,
@@ -23,14 +26,14 @@ router.get('/', Index)
 router.get('/signup', SignUp)
 router.get('/login', Login)
 router.get('/game', isLoggedIn, Game)
-router.get('/editAccount/:id', EditAccount)
-router.get('/editBiodata/:id', EditBiodata)
-router.get('/dashboard', Dashboard)
+router.get('/editAccount/:id',isLoggedIn, EditAccount)
+router.get('/editBiodata/:id', isLoggedIn, EditBiodata)
+router.get('/dashboard', isLoggedInAsAdmin,  Dashboard)
 router.post('/signup', RegisterFunction)
 router.post('/login', LoginFunction)
-router.post('/logout', LogoutFunction)
-router.post('/editBiodata/:id', EditBiodataFunction)
-router.post('/editAccount/:id', EditAccountFunction)
+router.post('/logout', isLoggedIn, LogoutFunction)
+router.post('/editBiodata/:id', isLoggedIn, EditBiodataFunction)
+router.post('/editAccount/:id', isLoggedIn, EditAccountFunction)
 
 
 
