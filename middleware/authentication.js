002cbb1af6e2efc41f3ff6ee7c5c952944ef8875
aanggaadc <aanggaadc@gmail.com>
2022-03-10 23:19:43 +0700

    adding some alert on authenticated middleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,67 +1,67 @@
 const jwt = require('jsonwebtoken')
 
 const isLoggedIn = (req, res, next) => {
-    const token = req.cookies.jwt
+  const token = req.cookies.jwt
 
-    if(token) {
-        jwt.verify(token, 'secret', (err, decodedToken) => {
-            if(err){
-                res.locals.user = null
-                req.flash('error', 'Token is Expired')
-                res.redirect('/login')
-            }else{
-                res.locals.user = decodedToken
-                console.log("Token Is :")
-                console.log(decodedToken)
-                next()
-            }
-        })        
-    } else {
+  if (token) {
+    jwt.verify(token, 'secret', (err, decodedToken) => {
+      if (err) {
         res.locals.user = null
-        req.flash('error', "You're not Login, Please Login")
+        req.flash('error', 'Token is Expired')
         res.redirect('/login')
-    }
+      } else {
+        res.locals.user = decodedToken
+        console.log("Token Is :")
+        console.log(decodedToken)
+        next()
+      }
+    })
+  } else {
+    res.locals.user = null
+    req.flash('error', "You're not Login, Please Login")
+    res.redirect('/login')
+  }
 }
 
 const isLoggedInAsAdmin = (req, res, next) => {
-    const token = req.cookies.jwt;
-    if (token) {
-      jwt.verify(token, "secret", (err, decodedToken) => {
-        if (err) {
-            req.flash('error', 'Token is Expired')
-            res.redirect('/login')
-        } else {
-          if (decodedToken.role !== 'SuperAdmin') {
-            req.flash('error', "You're Not Login As Admin")
-            res.redirect("/");
-          } else {
-            res.locals.user = decodedToken
-            console.log("Token Is :")
-            console.log(decodedToken)
-            next()
-          }
-        }
-      });
-    } else {
-        res.locals.user = null
-        req.flash('error', "You're not Login, Please Login")
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, "secret", (err, decodedToken) => {
+      if (err) {
+        req.flash('error', 'Token is Expired')
         res.redirect('/login')
-    }
+      } else {
+        if (decodedToken.role !== 'SuperAdmin') {
+          req.flash('error', "You're Not Login As Admin")
+          res.redirect("/");
+        } else {
+          res.locals.user = decodedToken
+          console.log("Token Is :")
+          console.log(decodedToken)
+          next()
+        }
+      }
+    });
+  } else {
+    res.locals.user = null
+    req.flash('error', "You're not Login, Please Login")
+    res.redirect('/login')
   }
+}
 
-  const checkNotAuthenticated = ( req, res , next ) =>{
-    const token = req.cookies.jwt;
+const checkNotAuthenticated = (req, res, next) => {
+  const token = req.cookies.jwt;
 
-    if(token){
-      res.redirect('/')
-    }else{
-      next()
-    }
-
+  if (token) {
+    req.flash('error', "You're Already Logged in")
+    res.redirect('/')
+  } else {
+    next()
   }
+}
 
 module.exports = {
-    isLoggedIn, 
-    isLoggedInAsAdmin,
-    checkNotAuthenticated
+  isLoggedIn,
+  isLoggedInAsAdmin,
+  checkNotAuthenticated
 }
